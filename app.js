2637e1afd844d0d1ad5d09675dcb7dd5b01d86a3
Addy Rodriguez <rdz.33.a@gmail.com>
2022-01-24 11:18:16 -0600

    App.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Dependencies
+const { resolveNaptr } = require('dns')
 const express =require('express')
 const { METHODS } = require('http')
+const { supportsColor } = require('supports-color')
 const app = (express)
 const PORT = 5000
 
@@ -17,32 +19,47 @@ app.use(methodOverride('_method'))
 
 
 // "Index" Route
-
+app.get('/Dogs', (req, res) => {
+    res.send("This is the index page")
+})
 
 
 
 // "New" Route
-
+app.get('/Dogs/new', (req, res) => {
+    res.send("This is the new route")
+})
 
 
 // "Show Route"
-
+app.get('/Dogs/:id', (req, res) =>{
+    res.send("This is the show route")
+})
 
 
 // "Edit Route"
-
+app.get('/Dogs/:id/edit', (req, res) => {
+    res.send("This is the edit route")
+})
 
 
 // "Create Route"
+app.post('/Dogs', (req, res) => {
+    res.send("This is the create route")
+})
 
 
 
 // "Destroy Route"
-
+app.delete('/Dogs/:id', (req, res) => {
+    res.send("This is the delete route")
+})
 
 
 // "Update Route"
-
+app.put('/fruits/:id', (req, res) => {
+    res.send("This is the update route")
+})
 
 
 // "Start the server"
