991c46efc465750be9bf3a595eb216b0be544484
paulua <paulavila54mac@gmail.com>
2022-01-26 15:35:42 -0600

    Added login still needs testing

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const app = express()
 
 const dogsController = require("./controllers/dogsC")
 const ownerController= require("./controllers/ownersC")
+const usersController= require("./controllers/usersC")
 
 const PORT = 3000
 const mongoose = require('mongoose')
 const URI = "mongodb://127.0.0.1:27017/dogOwnersDB"
 const methodOverride = require('method-override')
+
+
 const Owners = require('./models/Owners')
 
 // const owner = require('.models/Owners.js')
@@ -47,8 +50,15 @@ app.get('/Code-Pals',(req, res) => {
 
 })
 
+// our app login page
+app.get('/auth',(req, res) => {    
+      res.render("./Login-files/loginU.ejs",)  
+
+})
+app.use('/auth', usersController)
 app.use('/dogs', dogsController)
 app.use('/owners', ownerController)
+
 
 // "Start the server"
 app.listen(PORT, () => {
---
+++ controllers/usersC.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const router = express.Router()
+// insert the model (User)
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
+// Auth routing
+
+router.post('/login', (req,res)=>{
+    const credentials = req.body
+
+    User.findOne({username: credentials.username}, (err,foundUser)=>{
+        if(err){
+            res.send(err)
+        }else{
+            console.log(foundUser)
+            // res.send(foundUser)
+            if(!foundUser.password){
+                console.log('no user found')
+                res.redirect('/')
+            }else{
+                const testPassword = bcrypt.compareSync(credentials.password, foundUser.password)
+                // es.send(testPassword)
+                if(testPassword){
+                    console.log('logged in get ready for sessions')
+                    res.send('logged in ' + foundUser)
+                }else{
+                    res.send('incorrect password or username')
+                }
+            }
+        }
+    })
+})
+
+
+router.post('/register', (req,res)=>{
+    const userData = req.body
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(userData.password,salt)
+    function myFunction() {
+        var x = document.getElementById("myInput");
+        if (x.type === "password") {
+          x.type = "text";
+        } else {
+          x.type = "password";
+        }
+      }
+    console.log(userData,salt)
+
+    userData.password = hashedPassword
+    User.create(userData,(err,createdUser)=>{
+        if(err){
+            res.send(err)
+        }else{
+            res.send(createdUser)
+        }
+    })
+    //res.send('HashedPassword: ' + hashedPassword)
+})
+
+router.get('/logout', (req,res)=>res.send('logout route'))
+
+module.exports = router
